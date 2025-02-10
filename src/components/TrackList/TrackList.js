5940b71a99288274396fa263d57ecda08c17d42a
Costel Nechifor <me@hellocostel.com>
2025-02-10 17:13:16 +0100

    Create new component: TrackList

---
+++ src/components/TrackList/TrackList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './TrackList.module.css';
+import Track from '../Track/Track';
+
+function TrackList (results) {
+    return (
+        <div>
+            {results.map(result => {
+                rerurn (
+                    <Track />
+                )
+            })}
+        </div>
+    )
+}
+
+export default TrackList;
