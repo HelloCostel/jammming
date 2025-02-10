d0c13bfbdb635568e98109be1f96e7fa5b4490b0
Costel Nechifor <me@hellocostel.com>
2025-02-10 12:47:25 +0100

    Create new component: SearchBar

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import styles from './SearchBar.module.css';
+
+function SearchBar() {
+const [value, setValue] = useState('');
+
+const onChangeHandler = (e) => {
+    setValue(e.target.value);
+}
+
+    return (
+        <>
+            <input type='text' placeholder='Search' value={value} onChange={onChangeHandler} className={styles.searchbar}></input>
+        </>
+    )
+};
+
+export default SearchBar;
